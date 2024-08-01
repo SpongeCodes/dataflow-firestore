10bc4a813e4c1804aeb4ca7c091d8b8152d1fbfb
Upamanyu Mukharji <upamanyu.mukharji@hotmail.com>
2024-08-02 02:23:12 +0530

    add files

---
+++ src/main/java/StreamtoFirestore.java
@@ -0,0 +1,20 @@
+
+
+public class StreamtoFirestore {
+
+    public static void main(String[] args) {
+        // read from pub/sub and process from dataflow and send to firestore
+
+        // Step 1 subscribe from pub/sub pull all unacknowledged messages
+
+
+        // step 2 processing the messages with dataflow pipeline
+
+
+        // save to firestore asynchronously
+
+
+        // run dataflow job
+    }
+
+}
